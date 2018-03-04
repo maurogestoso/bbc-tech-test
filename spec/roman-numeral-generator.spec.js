c98cc1f005a946f3839e3753b63659d4d8f58b0b
Mauro Gestoso <maurogestoso@gmail.com>
2018-03-04 10:23:42 +0000

    Test numerals with multiple symbols for multiples of 1, 10, 100, 1000

--- spec/roman-numeral-generator.spec.js
+++ spec/roman-numeral-generator.spec.js
@@ -10,4 +10,15 @@ describe("generate", () => {
     expect(generate(500)).toBe("D");
     expect(generate(1000)).toBe("M");
   });
+
+  it("generates numerals with multiple symbols for multiples of [1, 10, 100, 1000]", () => {
+    expect(generate(2)).toBe("II");
+    expect(generate(3)).toBe("III");
+    expect(generate(20)).toBe("XX");
+    expect(generate(30)).toBe("XXX");
+    expect(generate(200)).toBe("CC");
+    expect(generate(300)).toBe("CCC");
+    expect(generate(2000)).toBe("MM");
+    expect(generate(3000)).toBe("MMM");
+  });
 });
