f0a5da645eb5fce69bd5cc25f1cfc3d78e23532e
Mauro Gestoso <maurogestoso@gmail.com>
2018-03-04 10:19:21 +0000

    Generate numerals for all individual symbols

--- spec/roman-numeral-generator.spec.js
+++ spec/roman-numeral-generator.spec.js
@@ -1,7 +1,13 @@
 const { generate } = require("../src/roman-numeral-generator");
 
 describe("generate", () => {
-  it("is a function", () => {
-    expect(typeof generate).toBe("function");
+  it("generates numerals for all the individual symbols", () => {
+    expect(generate(1)).toBe("I");
+    expect(generate(5)).toBe("V");
+    expect(generate(10)).toBe("X");
+    expect(generate(50)).toBe("L");
+    expect(generate(100)).toBe("C");
+    expect(generate(500)).toBe("D");
+    expect(generate(1000)).toBe("M");
   });
 });
--- src/roman-numeral-generator.js
+++ src/roman-numeral-generator.js
@@ -1,3 +1,24 @@
-const generate = number => {};
+const generate = number => {
+  let result = "";
 
-module.exports = { generate };
+  NUMERALS.forEach(({ value, symbol }) => {
+    while (value <= number) {
+      number -= value;
+      result += symbol;
+    }
+  });
+
+  return result;
+};
+
+const NUMERALS = [
+  { value: 1000, symbol: "M" },
+  { value: 500, symbol: "D" },
+  { value: 100, symbol: "C" },
+  { value: 50, symbol: "L" },
+  { value: 10, symbol: "X" },
+  { value: 5, symbol: "V" },
+  { value: 1, symbol: "I" }
+];
+
+module.exports = { generate, NUMERALS };
